9d66387ea0c8e68c5122980039fea358dc92604a
sumingzs11 <847366070@qq.com>
2019-05-02 15:37:06 +0800

    避免用户 刷新获取验证码 导致表单重复提交

---
+++ src/main/java/com/buaabetatwo/phyweb/interceptor/LockMethodInterceptor.java
@@ -0,0 +1,67 @@
+package com.buaabetatwo.phyweb.interceptor;
+
+import com.buaabetatwo.phyweb.annotation.LocalLock;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import static com.buaabetatwo.phyweb.PhywebApplication.myToken;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 本章先基于 本地缓存来做，后续讲解 redis 方案
+ *
+ * @author Levin
+ * @since 2018/6/12 0012
+ */
+@Aspect
+@Configuration
+public class LockMethodInterceptor {
+
+    private static final Cache<String, Object> CACHES = CacheBuilder.newBuilder()
+            // 最大缓存 100 个
+            .maximumSize(1000)
+            // 设置写缓存后 60 秒钟过期
+            .expireAfterWrite(60, TimeUnit.SECONDS)
+            .build();
+
+    @Around("execution(public * *(..)) && @annotation(com.buaabetatwo.phyweb.annotation.LocalLock)")
+    public Object interceptor(ProceedingJoinPoint pjp) {
+        myToken=1;
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();
+        LocalLock localLock = method.getAnnotation(LocalLock.class);
+        String key = getKey(localLock.key(), pjp.getArgs());
+        if (!StringUtils.isEmpty(key)) {
+            if (CACHES.getIfPresent(key) != null) {
+                myToken=0;
+               // throw new RuntimeException("请勿重复请求");
+            }
+            // 如果是第一次请求,就将 key 当前对象压入缓存中
+            CACHES.put(key, key);
+        }
+
+        try {
+            return pjp.proceed();
+        } catch (Throwable throwable) {
+          throw new RuntimeException("服务器异常");
+        } finally {
+            // CACHES.invalidate(key)
+        }
+    }
+
+
+    private String getKey(String keyExpress, Object[] args) {
+        for (int i = 0; i < args.length; i++) {
+            keyExpress = keyExpress.replace("arg[" + i + "]", args[i].toString());
+        }
+        return keyExpress;
+    }
+
+
+}
