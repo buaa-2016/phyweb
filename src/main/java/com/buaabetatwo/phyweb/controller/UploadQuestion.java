2ef42f39a3f82b9997be0dbb2029e37ff436ff49
MaybeSilent <804758962@qq.com>
2019-06-01 23:17:26 +0800

    添加插入题目后端接口，待测试

---
+++ src/main/java/com/buaabetatwo/phyweb/controller/UploadQuestion.java
@@ -0,0 +1,40 @@
+package com.buaabetatwo.phyweb.controller;
+
+import com.buaabetatwo.phyweb.mapper.QuestionMapper;
+import com.buaabetatwo.phyweb.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//用户上传题目后端接口，目前打算全部写在该文件中
+@Controller
+public class UploadQuestion {
+    private static Logger logger = LoggerFactory.getLogger(UploadQuestion.class);
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    //插入题目控制器
+    @ResponseBody
+    @PostMapping("/insertQues")
+    public Map insertQues(@RequestBody Question question) {
+        Map<String,String> ans = new HashMap<>();
+        try {
+            questionMapper.insertQuestion(question);
+        } catch (Exception e) {
+            logger.error("插入题目错误！错误为：" + e.toString());
+            ans.put("result","fail");
+        }
+        ans.put("result","success");
+        return ans;
+    }
+
+}
--- src/main/java/com/buaabetatwo/phyweb/mapper/QuestionMapper.java
+++ src/main/java/com/buaabetatwo/phyweb/mapper/QuestionMapper.java
@@ -2,6 +2,7 @@ package com.buaabetatwo.phyweb.mapper;
 
 import com.buaabetatwo.phyweb.model.Question;
 import com.buaabetatwo.phyweb.model.Report;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -16,5 +17,9 @@ public interface QuestionMapper {
 
     @Select("select * from questions")
     List<Question> findAll();
+
+    @Insert("insert into questions(title,A,B,C,D,answer,source,tag,other,hint,type) " +
+            "values(#{title},#{A},#{B},#{C},#{D},#{answer},#{source},#{tag},#{other},#{hint},#{type})")
+    void insertQuestion(Question question);
 
 }
