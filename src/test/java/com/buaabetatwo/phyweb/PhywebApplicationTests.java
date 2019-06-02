c592064b28915e3a4a675168838dc467347a35bf
MaybeSilent <804758962@qq.com>
2019-06-02 21:50:09 +0800

    添加填空与选择插入内容的单元测试

--- src/test/java/com/buaabetatwo/phyweb/PhywebApplicationTests.java
+++ src/test/java/com/buaabetatwo/phyweb/PhywebApplicationTests.java
@@ -18,10 +18,27 @@ public class PhywebApplicationTests {
 
     @Test
     public void insertQues() {
+        //添加填空题的测试
+
+        //下方三项内容为填空题必填内容
         Question ques = new Question();
         ques.setTitle("运动物体通过路径的___________叫做路程");
         ques.setAnswer("轨迹长度");
+        //其中Blank必填，作为判断该题是填空题的标准
         ques.setType("BLANK");
+        questionMapper.insertQuestion(ques);
+    }
+
+    @Test
+    public void insertQuesAnswer() {
+        Question ques = new Question();
+        //下方三项内容为简答题必填内容
+        ques.setTitle("在游泳池边向池底看去，感觉池水并不深，下水后才知道不是这么回事，试分析：为什么池水深度看起来比实际的浅？");
+
+        ques.setAnswer("光从一种介质斜射入另一种介质时要发生折射，人从空气看河底，实际看到的是河底的虚像，虚像的位置比实际河底的位置浅");
+
+        ques.setType("ANSWER");
+
         questionMapper.insertQuestion(ques);
     }
 
