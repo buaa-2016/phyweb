804d948e445d9235f46247730fdfde35eace1346
MaybeSilent <804758962@qq.com>
2019-06-02 21:39:19 +0800

    添加插入题库问题的测试

--- src/test/java/com/buaabetatwo/phyweb/PhywebApplicationTests.java
+++ src/test/java/com/buaabetatwo/phyweb/PhywebApplicationTests.java
@@ -1,14 +1,29 @@
 package com.buaabetatwo.phyweb;
 
-import com.buaabetatwo.phyweb.controller.ReportCenterController;
+
+import com.buaabetatwo.phyweb.mapper.QuestionMapper;
+import com.buaabetatwo.phyweb.model.Question;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PhywebApplicationTests {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Test
+    public void insertQues() {
+        Question ques = new Question();
+        ques.setTitle("运动物体通过路径的___________叫做路程");
+        ques.setAnswer("轨迹长度");
+        ques.setType("BLANK");
+        questionMapper.insertQuestion(ques);
+    }
 
     @Test
     public void contextLoads() {
